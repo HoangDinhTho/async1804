01b1d845ac0a4d0a215eebd51c2e991ba15aa30e
Huong <huongnguyenak96@gmail.com>
2019-05-11 20:56:34 +0700

    b

---
+++ b.js
@@ -0,0 +1,36 @@
+const request = require('request')
+const URL = 'http://localhost:3000/';
+
+function cong(a, b, fn){
+    if(isNaN(a) || isNaN(b) || !a || !b)
+        return fn(new Error('Invalid parameter(s)'))
+    const uri = `${URL}cong/${a}/${b}`;
+    request(uri,(error, response, body )=>{
+        if(error) return fn(error)
+        return fn(null, JSON.parse(body).result)
+    })
+}
+function nhan(a, b, fn){
+    if(isNaN(a) || isNaN(b) || !a || !b)
+        return fn(new Error('Invalid parameter(s)'))
+    const uri = `${URL}nhan/${a}/${b}`;
+    request(uri,(error, response, body )=>{
+        if(error) return fn(error)
+        return fn(null, JSON.parse(body).result)
+    })
+}
+function chia(a, b, fn){
+    if(isNaN(a) || isNaN(b) || !a || !b)
+        return fn(new Error('Invalid parameter(s)'))
+    const uri = `${URL}chia/${a}/${b}`;
+    request(uri,(error, response, body )=>{
+        if(error) return fn(error)
+        return fn(null, JSON.parse(body).result)
+    })
+}
+
+// (4+2)*5/2
+cong(4,2,(error,tong)=>{
+    if(error) return console.log(error.message)
+    console.log(tong)
+})
