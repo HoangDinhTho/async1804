2e02ea5df9ed06f89f52627d1d298dd9b2156d93
Huong <huongnguyenak96@gmail.com>
2019-05-11 21:26:33 +0700

    Promise

--- c.js
+++ c.js
@@ -3,10 +3,15 @@ const URL = 'http://localhost:3000/';
 
 function cong(a, b){
     const uri = `${URL}cong/${a}/${b}`;
-    request(uri,(error, response, body )=>{
-        if(error) {
-            
-        }
-        return 
+    return new Promise((resolve, reject)=>{
+        if(isNaN(a) || isNaN(b) || !a || !b)
+           return reject(new Error('Invalid'))
+           request(uri,(error, response, body )=>{
+            if(error) return reject(error)
+            return resolve(JSON.parse(body).result)
+        })
     })
 }
+cong(4,2)
+.then(r =>r)
+.catch(e=>console.log(e.message))
