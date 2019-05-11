b8308f082cffeaf7968f637cd37a019233b5d97f
Huong <huongnguyenak96@gmail.com>
2019-05-11 20:03:27 +0700

    request

---
+++ a.js
@@ -0,0 +1,6 @@
+let a = 1;
+setTimeout(()=>{
+    a = 2;
+    let b = 2
+}, 1000)
+console.log(a+b);
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const request = require('request')
 const URL = 'https://chain.so/api/v2/get_address_balance/BTC/';
 
+
+const uri = URL + '1NTxf1H9PoWCUWnKcdfLtFtgGWxRmWWq83';
+request.get(uri,(error, res, body)=>{
+    if(error) return console.log(error);
+    console.log(body)
+})
+
 /**
  * 1NTxf1H9PoWCUWnKcdfLtFtgGWxRmWWq83
  * 19Li3BpAigvtv2Z9ce4B5WwjXixfwqkgVy
